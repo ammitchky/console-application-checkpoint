ae40920c081549e3c15586b9e9e7165a145cf763
Andrew Mitschke <ammitchky@gmail.com>
2021-05-10 16:04:13 -0500

    added simple variable printouts

---
+++ NumberManipulator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NumberManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myInt = 12;
+            Console.WriteLine($"{myInt}");
+            Console.WriteLine($"{(myInt * myInt) + 14 - 3}");
+        }
+    }
+}
